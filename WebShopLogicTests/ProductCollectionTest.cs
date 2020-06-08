e9bdda36fff86bfb9e418d18a5456fff0bbf9946
dennycox <denny.cox@chello.nl>
2020-06-08 15:07:38 +0200

    Added ProductCollection Unit Test

---
+++ WebShopLogicTests/ProductCollectionTest.cs
@@ -0,0 +1,57 @@
+using Data.Models;
+using DataInterfaces.Repositories;
+using Logic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Threading.Tasks;
+
+namespace WebShopLogicTests
+{
+    [TestClass]
+    public class ProductCollectionTest
+    {
+        private readonly ProductCollection _sut; //Subject Under Test
+        private readonly Mock<IProductRepository> _productRepoMock = new Mock<IProductRepository>();
+
+        public ProductCollectionTest()
+        {
+            _sut = new ProductCollection(_productRepoMock.Object);
+        }
+
+        [TestMethod]
+        public void GetProductById_ProductExists_ReturnProduct()
+        {
+            //Arrange
+            var productId = 20;
+            var productName = "Actiefiguur Woody Toy Story";
+            var productDescription = "Actiefiguur Woody van Toy Story";
+            var productPrice = 17;
+            var productImagePath = "Woody.jpeg";
+            var productCategoryId = 2;
+
+            var productDto = new ProductDto
+            {
+                ProductID = productId,
+                Name = productName,
+                Description = productDescription,
+                Price = productPrice,
+                ImagePath = productImagePath,
+                CategoryID = productCategoryId,
+            };
+
+            _productRepoMock.Setup(x => x.GetProductById(productId)).Returns(productDto);
+
+            //Act
+            var product = _sut.GetProductById(productId);
+
+            //Assert
+            Assert.AreEqual(productId, product.ProductID);
+            Assert.AreEqual(productName, product.Name);
+            Assert.AreEqual(productDescription, product.Description);
+            Assert.AreEqual(productPrice, product.Price);
+            Assert.AreEqual(productImagePath, product.ImagePath);
+            Assert.AreEqual(productCategoryId, product.CategoryId);
+        }
+    }
+}
